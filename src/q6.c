c92be9b74732ac468cb710b8d3ef4ccc95209d4a
Sonam <sonurauniyar2062@gmail.com>
2025-09-05 21:49:55 +0545

    assignment completed

--- src/q6.c
+++ src/q6.c
@@ -1,1 +1,20 @@
 // Write a C program that prompts the user for a number and prints its multiplication table up to 10 using a do-while loop.
+#include <stdio.h>
+
+
+int main() {
+    int num, i = 1;
+     printf("Enter a number: ");
+     scanf("%d", &num);
+     do {
+        printf("%d x %d = %d\n", num, i, num * i);
+        i++;
+        } while (i <= 10);
+
+        return 0;
+}
+
+
+
+
+    
