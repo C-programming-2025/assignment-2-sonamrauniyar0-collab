ca320484cd138351eed8deeef7a7bac9d6a45320
Sonam <sonurauniyar2062@gmail.com>
2025-09-05 22:25:54 +0545

    assignment completed

--- src/q11.c
+++ src/q11.c
@@ -3,3 +3,22 @@
      ***
       *
 */
+#include <stdio.h>
+
+int main() 
+{
+    int i, j;
+    int rows = 3;
+     for(i = 1; i <= rows; i++) 
+     {
+         for(j = 1; j < i; j++) {
+            printf(" ");
+        }
+        for(j = 1; j <= 2*(rows-i) + 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+        
+    return 0;
+}
+    }
