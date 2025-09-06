95d045738ad380092e5abfafcd63603852fdbb43
Sonam <sonurauniyar2062@gmail.com>
2025-09-06 11:31:47 +0545

    assignment completed

--- src/q14.c
+++ src/q14.c
@@ -1,1 +1,20 @@
 // Write a C program that prompts the user for a positive integer and prints all the factors of that number using a for loop.
+#include <stdio.h>
+
+int main()
+ {
+    int num, i;
+     printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+      for(i = 1; i <= num; i++) {
+        if(num % i == 0) { 
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
--- src/q15.c
+++ src/q15.c
@@ -1,1 +1,31 @@
 // Write a C program that prompts the user for a positive integer and checks if it is a prime number using a while loop.
+#include <stdio.h>
+
+int main()
+{
+    int num, i=2;
+    int isprime =1;
+    printf("enter a positive integer:");
+    scanf("%d", &num);
+    if(num <=1)
+    {
+        isprime =0;
+    }
+    else{
+        while (i<=num/2)
+        {
+            if(num % i ==0)
+            {
+                isprime=0;
+                break;
+            }
+            i++;
+        }
+    }
+    if(isprime)
+    printf("%d is prime number.\n",num);
+    else
+     printf("%d is not a prime number.\n", num);
+     return 0;
+    }
+    
