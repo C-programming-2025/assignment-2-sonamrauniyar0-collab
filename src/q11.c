cfdf732c71633505ffc7bd6b95f00dcfecc1d5f4
Sonam <sonurauniyar2062@gmail.com>
2025-09-06 14:58:23 +0545

    assignment completed

--- src/q11.c
+++ src/q11.c
@@ -5,22 +5,19 @@
 */
 #include <stdio.h>
 
-int main() 
+int main()
 {
-    int i, j;
+    int i , j;
     int rows = 3;
-     for(i = 1; i <= rows; i++)
-     {
-         for(j = 1; j < i; j++)
-          {
-            printf(" ");
-        }
-        for(j = 1; j <= 2*(rows-i) + 1; j++)
-        {
-            printf("*");
-        }
-        printf("\n");
-     }    
-    return 0;
 
-    }
+    for(i = 1; i <= rows; i++)
+        {
+            for(j = 1; j <= 2*(rows - i) + 1; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+        return 0;
+    
+}
